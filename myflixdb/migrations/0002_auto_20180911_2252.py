41ecb103cf0625b299cc93244c11e0926d462ea2
rajmahal <rmahal25@gmail.com>
2018-09-11 16:22:32 -0700

    Seed file finished

--- myflixdb/migrations/0002_auto_20180911_2252.py
+++ myflixdb/migrations/0002_auto_20180911_2252.py
@@ -10,6 +10,14 @@ def seed(apps, schema_editor):
     Luke = User(username="userLuke", password="userLuke1")
     Raj.save()
     Luke.save()
+    sharp_objects = Flix(name="sharp objects", backdrop_path="aslkgjeigegegejowj", poster_path="a;oiegjegohgddd", movie_id=4355, overview="s;lgegeg gjeogjwg")
+    sharp_objects.save()
+    the_office = Flix(name="The Office", backdrop_path="asdasdada", poster_path="a;adadadadad", movie_id=1234, overview="a;adddd ddddaa")
+    the_office.save()
+    luke_like = Like(user=Luke, flix=sharp_objects)
+    luke_like.save()
+    raj_like = Like(user=Raj, flix=the_office)
+    raj_like.save()
 
 
 def fallow(apps, schema_editor):
